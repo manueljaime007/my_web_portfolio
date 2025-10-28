b37fd1f11ed144cc4b601bde0358584d250f65bc
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2025-10-28 20:51:19 +0000

    refactor: optimize server initialization and remove unused e2e tests

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,11 @@
-import { bootstrap } from '@src/main';
+import { bootstrap } from './src/main';
+
+let serverPromise: Promise<any>;
 
 export default async function handler(req: any, res: any) {
-  const server = await bootstrap(true);
+  if (!serverPromise) {
+    serverPromise = bootstrap(true);
+  }
+  const server = await serverPromise;
   return server(req, res);
 }
