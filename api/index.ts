e354169546bb372c7bcfad20d28b455aa6fd8d5b
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2025-10-28 19:46:52 +0000

    fix(build): ajustar configuração do Prisma e Vercel para build única do NestJS

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import { bootstrap } from './src/main';
+import { bootstrap } from '../src/main';
 
 export default async function handler(req: any, res: any) {
   const server = await bootstrap(true);
-  return server(req, res);
+  server(req, res);
 }
