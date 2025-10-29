413b8b73a76e5e2874759883d90c79da06755118
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2025-10-29 09:25:41 +0000

    update: main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,31 +5,34 @@ import express, { Express } from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import serverless from 'serverless-http';
 
-const server = express();
+const server: Express = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+// Marcar se o app já foi inicializado
+let nestAppPromise: Promise<any> | null = null;
 
-  app.setGlobalPrefix('api/v1');
-  app.enableCors();
+export const handler = async (req: any, res: any) => {
+  if (!nestAppPromise) {
+    nestAppPromise = (async () => {
+      const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+      app.setGlobalPrefix('api/v1');
+      app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Meu Portfólio Web - API')
-    .setDescription('Documentação da API do portfólio profissional do Manuel Jaime')
-    .setVersion('1.0')
-    .addTag('projects')
-    .addTag('contacts')
-    .addTag('experiences')
-    .build();
+      const config = new DocumentBuilder()
+        .setTitle('Meu Portfólio Web - API')
+        .setDescription('Documentação da API do portfólio profissional do Manuel Jaime')
+        .setVersion('1.0')
+        .addTag('projects')
+        .addTag('contacts')
+        .addTag('experiences')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/docs', app, document);
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api/v1/docs', app, document);
 
-  // **Não chamar app.listen() em serverless**
-  // const PORT = process.env.PORT || 3000;
-  // await app.listen(PORT);
-}
+      return app;
+    })();
+  }
 
-bootstrap();
-
-export const handler = serverless(server); // ✅ export para Vercel
+  await nestAppPromise; // garante que o Nest está pronto
+  return serverless(server)(req, res);
+};
