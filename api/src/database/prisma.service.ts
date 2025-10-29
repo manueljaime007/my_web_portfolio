c97d70c2de3b0bfa11adac50fc7a1152d539ced8
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2025-10-29 07:44:13 +0000

    update: prime service com singleton glocal

--- api/src/database/prisma.service.ts
+++ api/src/database/prisma.service.ts
@@ -1,11 +1,20 @@
+// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../../src/generated/client';
 
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaService | undefined;
+}
+
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: ['query'], // opcional, ajuda no debug
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
@@ -13,4 +22,13 @@ export class PrismaService
   async onModuleDestroy() {
     await this.$disconnect();
   }
+}
+
+// Singleton global para serverless
+export const prisma =
+  global.prisma ||
+  new PrismaService();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
 }
