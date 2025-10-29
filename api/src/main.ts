94b7adc87ec40f0730f04b77835e28b48367ce9c
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2025-10-29 12:24:28 +0000

    update: main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,38 +1,44 @@
+import serverless from 'serverless-http';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const server = express();
 const adapter = new ExpressAdapter(server);
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, adapter);
-  app.setGlobalPrefix('api/v1');
-  app.enableCors();
+let initialized = false;
 
-    // IMPLEMENTANDO SWAGGER <--
+const initApp = async () => {
+  if (!initialized) {
+    const app = await NestFactory.create(AppModule, adapter);
+    app.setGlobalPrefix('api/v1');
+    app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Meu Portfólio Web - API')
-    .setDescription(
-      'Documentação da API do portfólio profissional do Manuel Jaime',
-    )
-    .setVersion('1.0')
-    .addTag('projects')
-    .addTag('contacts')
-    .addTag('experiences')
-    .build();
+    // Swagger
+    const config = new DocumentBuilder()
+      .setTitle('Meu Portfólio Web - API')
+      .setDescription(
+        'Documentação da API do portfólio profissional do Manuel Jaime',
+      )
+      .setVersion('1.0')
+      .addTag('projects')
+      .addTag('contacts')
+      .addTag('experiences')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/docs', app, document);
-  // IMPLEMENTANDO SWAGGER  -->
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/docs', app, document);
 
+    await app.init();
+    initialized = true;
+  }
+};
 
-  await app.init();
-  return server; // retorna o express server imediatamente
-}
+const handler = async (req: any, res: any) => {
+  await initApp();
+  return serverless(server)(req, res);
+};
 
-export default bootstrap();
+export default handler;
